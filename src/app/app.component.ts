ee48175ddb62d6a484a57ff973bcbd6d44806f57
Elcio Abrahao <elcio.abrahao@viavarejo.com.br>
2021-04-17 18:28:57 -0300

    Commit aula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projeto-diretiva';
+
+textoBotao = "Esconder";
+esconder = false;
+
+pessoas = [
+  { nome: "José", idade: 18 },
+  { nome: "Maria", idade: 22 },
+  { nome: "Elcio", idade: 51 },
+  { nome: "Mario", idade: 35 },
+  { nome: "Nelson", idade: 12 }
+  ];
+
+alterarExibicao() {
+this.textoBotao = this.esconder ? "Esconder" : "Exibir";
+this.esconder = !this.esconder;
+}
+
+pegarCor(idade){
+  return idade >= 30 ? "red": "blue";
+}
 }
